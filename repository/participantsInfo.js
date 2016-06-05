07474de8557b351575fd3078793e0e6e75edfa8b
Milan Damen <github@darvit.nl>
2016-06-05 22:25:02 +0200

    Added child notes

--- repository/participantsInfo.js
+++ repository/participantsInfo.js
@@ -1,9 +1,8 @@
 var api = require('../modules/api');
 
 module.exports = {
-    // Return the list below
     getChildInformationList: function(req, res, callback){
-        api.get('/user/' + req.session.userid + '/children/?authToken=' + req.session.auth.auth_token, null, function(body){
+        api.get('/user/' + req.session.userid + '/children?authToken=' + req.session.auth.auth_token, null, function(body){
             callback(body);
         }, function(error){        
             if (error.statusCode === 417){
@@ -13,5 +12,6 @@ module.exports = {
             }
             callback(null);
         });
-    }, 
+    },
+    
 };
