d90da417ee7d1599b91047a88df8ed8fd20acb98
Kleinedruif <mike_hoskamp@hotmail.com>
2016-06-12 17:36:04 +0200

    some improvements

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -4,7 +4,7 @@ var api = require('../modules/api');
 
 module.exports = {
     getEventsPage: function(req, res, next){
-       
+        // Retrieve all active evenets
         eventsRepo.getEvents(req, res, function(events){       
            
             if (events == null) return mainController.render('events', req, res, {pageRoute: 'events', eventMsg: "Er zijn geen activiteiten bescikbaar"});
@@ -18,12 +18,13 @@ module.exports = {
             });
             
             if (currentEventId != null){
+                // Get the activities from an active evenets
                 eventsRepo.getActivities(req, res, currentEventId, function(oldActivities, upcommingActivities){
                         // Render events page
                         return mainController.render('events', req, res, {pageRoute: 'events', oldActivities: oldActivities, upcommingActivities: upcommingActivities});
                 });
             } else {
-                mainController.render('events', req, res, {pageRoute: 'events'});
+                mainController.render('events', req, res, {pageRoute: 'events', eventMsg: "Er zijn geen activiteiten bescikbaar"});
             }     
         });    
     }
--- repository/events.js
+++ repository/events.js
@@ -6,14 +6,13 @@ var messages = [];
 module.exports = {
     getEvents: function(req, res, callback){     
         api.get('/events/?authToken=' + req.session.auth.auth_token, null, function(body){          
-
             return callback(body);
         }, function(error){
             // Token expires afther 1 day
             if (error.status_code === 417){
                 return res.redirect('/sessieAfgelopen');
             } else {
-                console.log('message retrieved failed', error);
+                console.log('events retrieved failed', error);
             }
             return callback(null);
         });    
@@ -36,8 +35,8 @@ module.exports = {
                         activity.time = date2.getHours() + ":" + date2.getMinutes();
                     });
                 }
-                // Split in old and upcommingEvents
                 
+                // Split in old and upcommingEvents               
                 if (currentDate > date){
                     oldEvents.push(eventDay);
                 } else {
@@ -51,7 +50,7 @@ module.exports = {
             if (error.status_code === 417){
                 return res.redirect('/sessieAfgelopen');
             } else {
-                console.log('message retrieved failed', error);
+                console.log('event activities retrieved failed', error);
             }
             return callback(null);
         });    
@@ -63,10 +62,11 @@ setDatePrototypes = function(){
 
     var months = ['Januarie', 'Februarie', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
 
-    Date.prototype.getMonthName = function() {
-        return months[ this.getMonth() ];
+    // Set prototypes for easy access getting date
+    Date.prototype.getMonthName = function(){
+        return months[this.getMonth()];
     };
-    Date.prototype.getDayName = function() {
-        return days[ this.getDay() ];
+    Date.prototype.getDayName = function(){
+        return days[this.getDay()];
     };
 };
